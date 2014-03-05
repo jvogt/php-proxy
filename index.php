53d3eaca9d507884e8467fc5a622ccf02ca74926
Jeff Vogt <jeffrey_vogt@mcgraw-hill.com>
2014-03-04 21:21:06 -0800

    fixed parse error

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 include('proxy.php');
 
 $proxy = new Proxy();
-$proxy->forward(base64_decode(ltrim($_SERVER['REQUEST_URI'],'/'));
+$proxy->forward(base64_decode(ltrim($_SERVER['REQUEST_URI'],'/')));
